392d3456cb5295f31339d5e2430d77985fb9dd87
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-08-19 04:34:05 -0700

    done

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-document.getElementById('searchButton').addEventListener('click', () =>{
-    document.getElementById('resultsArea').style.display = 'block' ;
+const first = document.getElementById('searchButton')
+first.addEventListener('click', function(){
+    const second = document.getElementById('resultsArea')
+    second.style.display = 'block';
     fetch(`https://api.lyrics.ovh/suggest/${document.getElementById('search-input').value }`)
     .then(res => res.json())
     .then(data => {
        for(let i = 0 ; i <= 10 ; i++){
           let songName = data.data[i].title ; 
           let artistName = data.data[i].artist.name ;
-          document.getElementsByClassName('lyrics-name').item(i).innerText = `${songName}`;
-          document.getElementsByClassName('author-lead').item(i).innerText = `${artistName}`;
-          document.getElementsByClassName('get-lyrics').item(i).addEventListener('click' , () => {
+          const tin = document.getElementsByClassName('lyrics-name');
+          tin.item(i).innerText = `${songName}`;
+          const car = document.getElementsByClassName('author-lead');
+          car.item(i).innerText = `${artistName}`;
+          const pac = document.getElementsByClassName('get-lyrics');
+          pac.item(i).addEventListener('click' , function(){
              fetch(`https://api.lyrics.ovh/v1/${artistName}/${songName}`)
              .then(res => res.json())
              .then(data => { 
-                document.getElementsByClassName('lyrics-title').item(i).innerText = `${songName} by ${artistName}`;
-                document.getElementsByClassName('lyrics-body').item(i).innerText = data.lyrics ;
+                const choy = document.getElementsByClassName('lyrics-title');
+                choy.item(i).innerText = `${songName} by ${artistName}`;
+                const sat = document.getElementsByClassName('lyrics-body');
+                sat.item(i).innerText = data.lyrics ;
                 
              })
           })
